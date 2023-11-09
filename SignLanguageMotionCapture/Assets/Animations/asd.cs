0c2a60b8e57bcfdc69a0b716a75a488167e24ae1
byungwoo0601 <rlaquddn0601@naver.com>
2023-11-10 00:27:49 +0900

    20231110
    에니메이션 클립 지정설정
    메모리 초기화

--- SignLanguageMotionCapture/Assets/Animations/asd.cs
+++ SignLanguageMotionCapture/Assets/Animations/asd.cs
@@ -2,25 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.Animations;
+using Unity.VisualScripting;
+using System.Linq;
+
 public class asd : MonoBehaviour
 {
     Animator Anim;
-    string stateName = "NewEmptyState";
-    public AnimatorController ac;
-    // Start is called before the first frame update
-    private void AddEmptyState()
-    {
-        AnimatorController ac = Anim.runtimeAnimatorController as AnimatorController;
-        AnimatorStateMachine stateMachine = ac.layers[0].stateMachine;
-        AnimatorState emptyState = stateMachine.AddState(stateName);
-        emptyState.motion = null; // Empty State이므로 모션은 null로 설정
-    }
+    AnimatorController ac;
+    public AnimationClip[] animationClip;
+    public AnimatorState[] emptyState;
+    AnimatorStateMachine stateMachine;
 
+    public string stateName = "";
+    public string clipName = "";
     void Start()
     {
+        animationClip = new AnimationClip[2];
+        emptyState = new AnimatorState[2];
+
         Anim = GetComponent<Animator>();
-        Debug.Log(ac.animationClips[0].length);
-        Debug.Log(ac.animationClips[1].length);
-        AddEmptyState();
+        AddEmptyStates();
+        SetupTransitions();
+    }
+
+    private void AddEmptyStates()
+    {
+        ac = Anim.runtimeAnimatorController as AnimatorController;
+        stateMachine = ac.layers[0].stateMachine;
+
+        string[] clipNames = { "Armature_Extracted motion_Armature", "HipHopDancing" };
+        AnimationClip[] animationClips = new AnimationClip[clipNames.Length];
+
+        for (int i = 0; i < clipNames.Length; i++)
+        {
+            animationClips[i] = Resources.Load(clipNames[i]) as AnimationClip;
+
+            emptyState[i] = stateMachine.AddState("NewEmptyState" + i);
+            emptyState[i].motion = animationClips[i];
+        }
+    }
+
+    void SetupTransitions()
+    {
+        for (int i = 0; i < emptyState.Length - 1; i++)
+        {
+            AnimatorStateTransition transition = emptyState[i].AddTransition(emptyState[i + 1]);
+            transition.hasExitTime = true;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        RemoveEmptyState();
+    }
+
+    private void RemoveEmptyState()
+    {
+        foreach (AnimatorState state in emptyState)
+        {
+            stateMachine.RemoveState(state);
+        }
     }
 }
