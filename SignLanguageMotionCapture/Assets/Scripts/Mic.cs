581d9c8e5c61854669e8f8a0ea16711f30df378c
hogenie <shinkang1013@gmail.com>
2023-11-11 22:19:45 +0900

    20231111
    Mic2

--- SignLanguageMotionCapture/Assets/Scripts/Mic.cs
+++ SignLanguageMotionCapture/Assets/Scripts/Mic.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Mic : MonoBehaviour
 {
     AudioSource aud;
     private string microphoneID = null;
-    private int recordingLengthSec = 20;
+    private int recordingLengthSec = 100;
     private int recordingHZ = 44100;
+    System.Diagnostics.Stopwatch watch;
 
     // Start is called before the first frame update
     void Start()
@@ -18,22 +20,25 @@ public class Mic : MonoBehaviour
     }
     public void StartRec()
     {
+        watch = new System.Diagnostics.Stopwatch();
+        watch.Start();
         Debug.Log("start recording");
         aud.clip = Microphone.Start(microphoneID, false, recordingLengthSec, recordingHZ);
     }
 
     public void StopRec()
     {
-        if(Microphone.IsRecording(microphoneID))
+        if (Microphone.IsRecording(microphoneID))
         {
             Microphone.End(microphoneID);
+            watch.Stop();
+
+            float time = watch.ElapsedMilliseconds / 1000f;
+            Debug.Log(time + "second");
 
             Debug.Log("stop recording");
+            TrimSilence(aud.clip, time);
             Debug.Log(aud.clip.length);
-            float[] samples = new float[aud.clip.samples];
-            aud.clip.GetData(samples, 0);
-            float[] cutSamples = new float[0];
-            //Array.Copy(samples, cutSamples, cutSamples.Length - 1);
             if (aud.clip == null)
             {
                 Debug.Log("nothing recorded");
@@ -46,5 +51,28 @@ public class Mic : MonoBehaviour
     {
         Debug.Log("play recording");
         aud.Play();
+    }
+    private AudioClip TrimSilence(AudioClip clip, float endTime)
+    {
+        float[] samples = new float[clip.samples * clip.channels];
+        clip.GetData(samples, 0);
+
+        int endIndex = (int)(endTime * clip.frequency) * clip.channels;
+
+        int i;
+        for (i = endIndex - 1; i>=0;i--)
+        {
+            if (Mathf.Abs(samples[i]) > 0.1f)
+            {
+                break;
+            }
+        }
+        float[] trimmedSamples = new float[i + 1];
+        Array.Copy(samples, trimmedSamples, i + 1);
+
+        AudioClip trimmedClip = AudioClip.Create("trimmed", trimmedSamples.Length / clip.channels, clip.channels, clip.frequency, false);
+        trimmedClip.SetData(trimmedSamples, 0);
+
+        return trimmedClip;
     }
 }
