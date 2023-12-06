87d3336b2a3b8b6b5f12089f6604d35227f5cef8
hogenie <shinkang1013@gmail.com>
2023-12-06 23:49:33 +0900

    20231206
    ...

--- SignLanguageMotionCapture/Assets/Scripts/ClipCreation.cs
+++ SignLanguageMotionCapture/Assets/Scripts/ClipCreation.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Animations;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ClipCreation : MonoBehaviour
 {
@@ -11,8 +10,6 @@ public class ClipCreation : MonoBehaviour
     public AnimationClip[] animationClip;
     public AnimatorState[] emptyState;
     AnimatorStateMachine stateMachine;
-
-    public Text resultText;
 
     public string stateName = "";
     public string clipName = "";
@@ -31,15 +28,14 @@ public class ClipCreation : MonoBehaviour
         ac = Anim.runtimeAnimatorController as AnimatorController;
         stateMachine = ac.layers[0].stateMachine;
 
-        List<string> words = SentenceToWords.words;
+        string[] clipNames = { "Armature_Extracted motion_Armature", "HipHopDancing" };
+        AnimationClip[] animationClips = new AnimationClip[clipNames.Length];
 
-        AnimationClip[] animationClips = new AnimationClip[words.Count-1];
-
-        for (int i = 0; i < words.Count-1; i++)
+        for (int i = 0; i < clipNames.Length; i++)
         {
-            animationClips[i] = Resources.Load(words[i]) as AnimationClip;
+            animationClips[i] = Resources.Load(clipNames[i]) as AnimationClip;
 
-            emptyState[i] = stateMachine.AddState(words[i]);
+            emptyState[i] = stateMachine.AddState("NewEmptyState" + i);
             emptyState[i].motion = animationClips[i];
         }
     }
--- SignLanguageMotionCapture/Assets/Scripts/test/STS_Manager.cs
+++ SignLanguageMotionCapture/Assets/Scripts/test/STS_Manager.cs
@@ -3,27 +3,58 @@ using System.Collections.Generic;
 using Google.Cloud.Speech.V1;
 using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
 namespace FrostweepGames.Plugins.GoogleCloud.StreamingSpeechRecognition.Examples
 {
 	public class STS_Manager : MonoBehaviour
 	{
+		public static STS_Manager instance;
+
 		private GCStreamingSpeechRecognition_ _speechRecognition;
 
-		public Button RecordButton_1,
-						RecordButton_2;
+		[Header("Buttons")]
+		public Button RecordButton_1;
+		public Button RecordButton_2;
+		public Button DeleteButton;
+		public Button TestButton;
+		public Button EditButton;
 
+		[Header("Prefabs")]
+		public GameObject Text_prefab;
+		public GameObject InputField_prefab;
+
+		[Header("GameObject")]
+		public GameObject Clicked_Image;
+		public GameObject Text_parent;
+		public GameObject InputField_parent;
+
+		[Header("Text")]
 		public Text _resultText;
 
+		[Header("TMP_InputField")]
+		public TMP_InputField inputField;
+
+		[Header("ScrollRect")]
 		public ScrollRect scrollRect;
 
-		public float voiceDetectionThreshold = 0.02f;
+		[Header("Test")]
+		public string temp_Text;
+		public string get_Text;
+		public int count;
+		public GameObject test_1;
+		public GameObject test_2;
 
-		public int record_count = 0;
-		public string temp_text;		
+		List<Text> Text_List = new List<Text>();
+		List<TMP_InputField> InputField_List = new List<TMP_InputField>();
 
-		private void Start()
+		private void Awake()
+        {
+			instance = this;
+		}
+        private void Start()
 		{
+			count = 1;
 			_speechRecognition = GCStreamingSpeechRecognition_.Instance;
 			_speechRecognition.StreamingRecognitionStartedEvent += StreamingRecognitionStartedEventHandler;
 			_speechRecognition.StreamingRecognitionFailedEvent += StreamingRecognitionFailedEventHandler;
@@ -33,22 +64,37 @@ namespace FrostweepGames.Plugins.GoogleCloud.StreamingSpeechRecognition.Examples
 
 			RecordButton_1.onClick.AddListener(StartRecordButtonOnClickHandler);
 			RecordButton_2.onClick.AddListener(StopRecordButtonOnClickHandler);
+			DeleteButton.onClick.AddListener(DeleteButtonOnClickHandler);
+			TestButton.onClick.AddListener(TestButtonOnClickEventHandler);
+			EditButton.onClick.AddListener(EditButtonOnClickHandler);
 
 			_speechRecognition.SetMicrophoneDevice(_speechRecognition.GetMicrophoneDevices()[0]);
 		}
-
-		private void OnDestroy()
+        private void OnDestroy()
 		{
 			_speechRecognition.InterimResultDetectedEvent -= InterimResultDetectedEventHandler;
 			_speechRecognition.FinalResultDetectedEvent -= FinalResultDetectedEventHandler;
 		}
+		private void EditButtonOnClickHandler()
+        {
 
+        }
+		private void DeleteButtonOnClickHandler()
+        {
+			if(Clicked_Image==null)
+			{
+				Debug.Log("오브젝트 선택 안 됐습니다.");
+			}
+			Destroy(Clicked_Image);
+			count--;
+        }
 		private void StartRecordButtonOnClickHandler()
 		{
-			if (record_count == 0) 
-            {
-				_resultText.text = string.Empty;
-            }
+			if (_resultText == null)
+			{
+				Instantiate(Text_prefab, Text_parent.GetComponent<Transform>());
+				Debug.Log("입력될 텍스트 오브젝트가 없어서 새로운 오브젝트를 생성하였습니다.");
+			}
 
 			List<List<string>> context = new List<List<string>>();
 
@@ -64,6 +110,11 @@ namespace FrostweepGames.Plugins.GoogleCloud.StreamingSpeechRecognition.Examples
 				}
 			}
 
+			if (count == 0)
+			{
+				_resultText.text = string.Empty;
+			}
+
 			_speechRecognition.config.recognitionSettings.speechContexts = contexts;
 
 			_speechRecognition.StartStreamingRecognition(_speechRecognition.config.recognitionSettings);
@@ -71,9 +122,8 @@ namespace FrostweepGames.Plugins.GoogleCloud.StreamingSpeechRecognition.Examples
 			RecordButton_2.gameObject.SetActive(true);
 			RecordButton_1.gameObject.SetActive(false);
 
-			temp_text = _resultText.text;
+			temp_Text = _resultText.text;
 		}
-
 		private async void StopRecordButtonOnClickHandler()
 		{
 			await _speechRecognition.StopStreamingRecognition();
@@ -81,14 +131,34 @@ namespace FrostweepGames.Plugins.GoogleCloud.StreamingSpeechRecognition.Examples
 			RecordButton_1.gameObject.SetActive(true);
 			RecordButton_2.gameObject.SetActive(false);
 
-			record_count++;
+			count++;
+
+			pushInputField(_resultText.text);
+			DuplicationObject();
+			StringEmpty();
 		}
 		private void StreamingRecognitionStartedEventHandler()
 		{
 			RecordButton_2.interactable = true;
 			RecordButton_1.interactable = false;
 		}
-
+		private void TestButtonOnClickEventHandler()
+		{
+            Instantiate(Text_prefab, Text_parent.GetComponent<Transform>());
+            count++;
+            _resultText = Text_parent.GetComponent<Transform>().GetChild(count - 1).GetComponentInChildren<Text>();
+            _resultText.text = string.Empty;
+            //if(test_1.activeSelf == true)
+            //         {
+            //	test_1.SetActive(false);
+            //	test_2.SetActive(true);
+            //}
+            //else if(test_2.activeSelf == true)
+            //{
+            //	test_1.SetActive(true);
+            //	test_2.SetActive(false);
+            //}
+        }
 		private void StreamingRecognitionFailedEventHandler(string error)
 		{
 			_resultText.text = $"<color=red>Start record Failed due to: {error}.</color>";
@@ -96,39 +166,41 @@ namespace FrostweepGames.Plugins.GoogleCloud.StreamingSpeechRecognition.Examples
 			RecordButton_2.interactable = false;
 			RecordButton_1.interactable = true;
 		}
-
 		private void StreamingRecognitionEndedEventHandler()
 		{
 			RecordButton_2.interactable = false;
 			RecordButton_1.interactable = true;
 		}
-
 		private void InterimResultDetectedEventHandler(SpeechRecognitionAlternative alternative)
 		{
-			if(record_count == 0)
-            {
-				_resultText.text = $"{alternative.Transcript}";
-			}
-            else
-			{
-				_resultText.text = $"{temp_text}\n{alternative.Transcript}";
-			}
+			_resultText.text = $"{alternative.Transcript}";
 
 			scrollRect.verticalNormalizedPosition = 0f;
 		}
-
 		private void FinalResultDetectedEventHandler(SpeechRecognitionAlternative alternative)
 		{
-			if (record_count == 0 || record_count == 1)
-			{
-				_resultText.text = $"{alternative.Transcript}";
-			}
-			else
-			{
-				_resultText.text = $"{temp_text}\n{alternative.Transcript}";
-			}
-
+			_resultText.text = $"{alternative.Transcript}";
+			
 			scrollRect.verticalNormalizedPosition = 0f;
+		}
+		private void DuplicationObject()
+		{
+			Instantiate(Text_prefab, Text_parent.GetComponent<Transform>());
+			_resultText = Text_parent.GetComponent<Transform>().GetChild(count - 1).GetComponentInChildren<Text>();
+			Text_List.Add(_resultText);
+			Debug.Log($"{Text_List.Count} 텍스트 리스트 숫자");
+		}
+		private void StringEmpty()
+        {
+			_resultText.text = string.Empty;
+		}
+		private void pushInputField(string text)
+        {
+			inputField.text = text;
+			Instantiate(InputField_prefab, InputField_parent.GetComponent<Transform>());
+			inputField = InputField_parent.GetComponent<Transform>().GetChild(count - 1).GetComponentInChildren<TMP_InputField>();
+			InputField_List.Add(inputField);
+			Debug.Log($"{InputField_List.Count} 인 풋 텍스트 리스트 숫자");
 		}
 	}
 }
